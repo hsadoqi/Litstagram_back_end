593f5c7a6f034216a17f5335fc743104e4c659c7
Hanaa Sadoqi <hsadoqi@gmail.com>
2018-11-30 17:45:13 -0500

    edited serializers

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,7 +1,4 @@
 class ImageSerializer < ActiveModel::Serializer
   attributes :id, :caption, :img, :likes, :comments, :commenters, :likers
   belongs_to :poster
-  # has_many :likes, serializer: LikeSerializer
-  # has_many :comments, serializer: CommentSerializer
-  # has_many :followers, serializer: UserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :password, :posts, :liked_images, :comments_made
-  # has_many :posts, serializer: ImageSerializer 
-  # has_many :liked_images, serializer: LikeSerializer
-  # has_many :image_comments, serializer: CommentSerializer
+  attributes :id, :first_name, :last_name, :username, :password, :posts, :liked_images, :comments_made, :comments, :likes
 end
