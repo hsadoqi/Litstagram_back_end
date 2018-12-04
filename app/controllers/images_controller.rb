a1aa6e341bedc2d61a5fa3efff38f36c6c26ccb1
github username <github email address>
2018-12-04 15:30:15 -0500

    working with cloudinary

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,49 +1,53 @@
 class ImagesController < ApplicationController
     before_action :set_image, only: [:show, :update, :destroy]
 
-    # GET /images 
-    def index 
-        @images = Image.all 
+    # GET /images
+    def index
+        @images = Image.all
 
         render json: @images
-    end 
+    end
 
     # GET /images/:id
-    def show 
+    def show
         render json: @image
-    end 
+    end
 
     # POST /images
-    def create 
-        @image = Image.new(image_params)
-        if @image.save 
-            render json: @image 
-        else 
-            render json: @image.errors.full_messages 
-        end 
-    end 
+    def create
+        @image = Image.create(image_params)
+          byebug
+        if @image.valid?
+          # byebug
+          render json: @image
+
+        else
+            render json: @image.errors.full_messages
+        end
+
+    end
 
     # PATCH/PUT /images/:id
-    def update 
+    def update
         if @image.update(image_params)
-            render json: @image 
-        else 
+            render json: @image
+        else
             render json: @image.errors.full_messages
-        end 
-    end 
+        end
+    end
 
     # DELETE /images/:id
     def destroy
-        @image.destroy 
-    end 
+        @image.destroy
+    end
 
-    private 
+    private
 
-    def set_image 
+    def set_image
         @image = Image.find(params[:id])
-    end 
+    end
 
     def image_params
-        params.require(:image).permit(:caption, :img)
+        params.require(:image).permit(:caption, :img, :poster_id)
     end
 end
